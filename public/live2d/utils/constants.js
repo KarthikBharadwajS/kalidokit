7d4f8ee0182c6e7635338b1a1c2b36b6c8f69e7c
Karthik <karthik.bharadwaj@pickcel.com>
2022-07-23 21:20:21 +0530

    live 2d and 3d

---
+++ public/live2d/utils/constants.js
@@ -0,0 +1,9 @@
+export const modelsList = [{
+    name: "Hiyori",
+    path: "./models/hiyori/hiyori_pro_t10.model3.json",
+    icon: ""
+}, {
+    name: "Maou",
+    path: "./models/mao_pro_en/mao_pro_t02.model3.json",
+    icon: ""
+}];
